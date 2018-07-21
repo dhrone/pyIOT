5f49467c41bc0614aabc127419a137bc9a97bad5
dhrone <ron@ritchey.org>
2018-07-21 17:23:18 +0000

    Example working

--- example.py
+++ example.py
@@ -1,6 +1,8 @@
-import pyIOT
+from pyIOT import Thing, Component
 
-class AVM(Device):
+import serial
+
+class AVM(Component):
 
     def __init__(self, port, baud):
         self._ser = serial.Serial(port, baud, timeout=0.25)
@@ -16,7 +18,7 @@ class AVM(Device):
         else:
             return 'P1P?\n'
 
-    @Device.deviceToProperty('powerState', '^P1P([0-1])$')
+    @Component.componentToProperty('powerState', '^P1P([0-1])$')
     def avmToPowerState(self, property, value):
         assert (property == 'powerState'), 'Wrong property received: ' + property
         val = { '1': 'ON', '0': 'OFF' }.get(value)
@@ -24,7 +26,7 @@ class AVM(Device):
             return val
         raise ValueError('{0} is not a valid value for property {1}'.format(value, property))
 
-    @Device.deviceToProperty('input', '^P1S([0-9])$')
+    @Component.componentToProperty('input', '^P1S([0-9])$')
     def avmToInput(self, property, value):
         assert (property == 'input'), 'Wrong property received: ' + property
         val = { '0': 'CD', '3': 'TAPE', '5': 'DVD', '6': 'TV', '7': 'SAT', '8': 'VCR', '9': 'AUX' }.get(value)
@@ -32,7 +34,7 @@ class AVM(Device):
             return val
         raise ValueError('{0} is not a valid value for property {1}'.format(value, property))
 
-    @Device.deviceToProperty('volume', '^P1VM([+-][0-9]{1,2}(?:[\\.][0-9]{1,2})?)$')
+    @Component.componentToProperty('volume', '^P1VM([+-][0-9]{1,2}(?:[\\.][0-9]{1,2})?)$')
     def avmToVolume(self, property, value):
         assert (property == 'volume'), 'Wrong property received: ' + property
         try:
@@ -46,7 +48,7 @@ class AVM(Device):
             # volume greater than max array value
             return 100
 
-    @Device.deviceToProperty('muted', '^P1M([0-1])$')
+    @Component.componentToProperty('muted', '^P1M([0-1])$')
     def avmToMuted(self, property, value):
         assert (property == 'muted'), 'Wrong property received: ' + property
         val = { '1': True, '0': False }.get(value)
@@ -54,26 +56,26 @@ class AVM(Device):
             return val
         raise ValueError('{0} is not a valid value for property {1}'.format(value, property))
 
-    @Device.deviceToProperty(['input', 'volume', 'muted'], '^P1S([0-9])V([+-][0-9]{2}[\\.][0-9])M([0-1])D[0-9]E[0-9]$')
+    @Component.componentToProperty(['input', 'volume', 'muted'], '^P1S([0-9])V([+-][0-9]{2}[\\.][0-9])M([0-1])D[0-9]E[0-9]$')
     def avmcombinedResponse(self, property, value):
         assert (property in ['input','volume', 'muted']), 'Wrong property received: {0}'.format(property)
         return { 'input': self.avmToInput, 'volume': self.avmToVolume, 'muted': self.avmToMuted }.get(property)(property, value)
 
-    @Device.propertyToDevice('powerState', 'P1P{0}')
+    @Component.propertyToComponent('powerState', 'P1P{0}')
     def powerStateToAVM(self, value):
         val = { 'ON': '1', 'OFF': '0' }.get(value)
         if val:
             return val
         raise ValueError('{0} is not a valid powerState'.format(value))
 
-    @Device.propertyToDevice('input', 'P1S{0}')
+    @Component.propertyToComponent('input', 'P1S{0}')
     def inputToAVM(self, value):
         val = { 'CD': '0', 'TAPE': '3', 'DVD': '5', 'TV': '6', 'SAT': '7', 'VCR': '8', 'AUX': '9' }.get(value)
         if val:
             return val
         raise ValueError('{0} is not a valid input'.format(value))
 
-    @Device.propertyToDevice('volume', 'P1VM{0}')
+    @Component.propertyToComponent('volume', 'P1VM{0}')
     def volumeToAVM(self, value):
         if type(value) is int:
             value = int(value/10)
@@ -81,14 +83,14 @@ class AVM(Device):
             return self.volarray[value]
         raise ValueError('{0} is not a valid volume'.format(value))
 
-    @Device.propertyToDevice('muted', 'P1M{0}')
+    @Component.propertyToComponent('muted', 'P1M{0}')
     def muteToAVM(self, value):
         val = { True: '1', False: '0' }.get(value)
         if val:
             return val
         raise ValueError('{0} is not a valid muted value'.format(value))
 
-class Epson1080UB(Device):
+class Epson1080UB(Component):
 
     def __init__(self, port, baud):
         self._ser = serial.Serial(port, baud, timeout=0.25)
@@ -111,7 +113,7 @@ class Epson1080UB(Device):
     def ready(self):
         return True if self.properties['projPowerState'] in ['ON', 'OFF', 'UNKNOWN'] else False
 
-    @Device.deviceToProperty('projPowerState', '^PWR=([0-9]{2})$')
+    @Component.componentToProperty('projPowerState', '^PWR=([0-9]{2})$')
     def toProjPowerState(self, property, value):
         assert (property == 'projPowerState'), 'Wrong property received: ' + property
         val = { '00': 'OFF', '01': 'ON', '02': 'WARMING', '03': 'COOLING', '04': 'STANDBY', '05': 'ABNORMAL' }.get(value)
@@ -119,7 +121,7 @@ class Epson1080UB(Device):
             return val
         raise ValueError('{0} is not a valid value for property {1}'.format(value, property))
 
-    @Device.deviceToProperty('projInput', '^SOURCE=([a-zA-Z0-9]{2})$')
+    @Component.componentToProperty('projInput', '^SOURCE=([a-zA-Z0-9]{2})$')
     def toProjInput(self, property, value):
         assert (property == 'projInput'), 'Wrong property received: ' + property
         val = { '30': 'HDMI1', 'A0': 'HDMI2', '41': 'VIDEO', '42': 'S-VIDEO' }.get(value)
@@ -127,13 +129,13 @@ class Epson1080UB(Device):
             return val
         raise ValueError('{0} is not a valid value for property {1}'.format(value, property))
 
-    @Device.propertyToDevice('projPowerState', 'PWR {0}\r')
+    @Component.propertyToComponent('projPowerState', 'PWR {0}\r')
     def projPowerStateToProj(self, value):
         if value in ['ON', 'OFF']:
             return value
         raise ValueError('{0} is not a valid powerState'.format(value))
 
-    @Device.propertyToDevice('projInput', 'SOURCE {0}\r')
+    @Component.propertyToComponent('projInput', 'SOURCE {0}\r')
     def projInputToProj(self, value):
         val = { 'HDMI1': '30', 'HDMI2': 'A0', 'VIDEO': '41', 'S-VIDEO': '42' }.get(value)
         if val:
@@ -141,7 +143,7 @@ class Epson1080UB(Device):
         raise ValueError('{0} is not a valid input'.format(value))
 
 
-class denTVThing(Thing):
+class condoTVThing(Thing):
 
     def onChange(self, updatedProperties):
         rv = []
@@ -155,11 +157,9 @@ class denTVThing(Thing):
 if __name__ == u'__main__':
 
     try:
-        denAVM = AVM('/dev/ttyUSB1',9600)
-        denEpson = Epson1080UB('/dev/ttyUSB0',9600)
+        condoAVM = AVM('/dev/ttyUSB0',9600)
 
-        denTV = denTVThing(endpoint='aamloz0nbas89.iot.us-east-1.amazonaws.com', thingName='denTVThing', rootCAPath='root-CA.crt', certificatePath='denTVThing.crt', privateKeyPath='denTVThing.private.key', region='us-east-1', devices=[denAVM,denEpson])
-        denTV.start()
+        condoTV = condoTVThing(endpoint='aamloz0nbas89.iot.us-east-1.amazonaws.com', thingName='condoTV', rootCAPath='root-CA.crt', certificatePath='condoTV.crt', privateKeyPath='condoTV.private.key', region='us-east-1', components=[condoAVM])
+        condoTV.start()
     except KeyboardInterrupt:
-        denAVM.exit()
-        denEpson.exit()
+        condoAVM.exit()
